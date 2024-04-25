11497c4d23cb25620c21580684c5536dd22f67e6
Raymond House <raymondhouse2003@gmail.com>
2024-04-24 21:48:18 -0700

    initial

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className={"fixed flex flex-col justify-center items-center space-y-4"}>
+            <h1 className={"text-6xl font-semibold font-mono"}>Word Pop</h1>
+        </div>
+    )
+}
